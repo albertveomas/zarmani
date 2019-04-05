f8477db92d1204b566ad81ad6213fe83d27fda42
hnin wai <hninwaioo56@gmail.com>
2019-04-05 11:42:25 +0630

    routes

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const user = require('./user');
 const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
 const db = "mongodb://hninwaioo:hninwaioo123@ds131296.mlab.com:31296/zarmani";
-const admin = require('./admin/index.js');
-const member = require('./member/index.js');
-const staff = require('./staff/index.js');
+const admin = require('./routes/admin/index.js');
+const member = require('./routes/member/index.js');
+const staff = require('./routes/staff/index.js');
 
 
 
