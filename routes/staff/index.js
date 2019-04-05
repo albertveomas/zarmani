5781c796e8890ed98555bee39ae45b8129c3e638
hnin wai <hninwaioo56@gmail.com>
2019-04-05 11:47:58 +0630

    keys

--- routes/staff/index.js
+++ routes/staff/index.js
@@ -6,7 +6,8 @@ var router = express.Router()
 router.post('/create-member', function (req, res) {
   let id = req.body.ID;
   let name = req.body.name;
-  let messengerId = req.body.messenger_user_id;
+  let messengerId = req.body["messenger user id"];
+  console.log(`Objects are ${Objects.keys(req.body)}`);
   console.log(`id is ${id}, name is ${name}, messenger is ${messengerId}`)
 })
 // define the about route
