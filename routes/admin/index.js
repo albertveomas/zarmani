f2ddb7f3ddd6c66469244941ab86e8174464f7d6
hnin wai <hninwaioo56@gmail.com>
2019-04-06 12:37:57 +0630

    staff

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,14 +1,41 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Staff = require('../../schema/StaffCreate');
 
 
 // define the home page route
-router.post('/', function (req, res) {
-  res.send('Birds home page')
-})
-// define the about route
-router.get('/about', function (req, res) {
-  res.send('About birds')
+router.post('/staff-register', function (req, res) {
+  let staffId = req.body.id;
+  let Name = req.body.name;
+  let code = req.body.code;
+  let phone = req.body.phone;
+  let staffConfirm = false;
+
+
+  Staff.find({staffId}, function(err, staff){
+  	if(staff[0] === undefined){
+  		Staff.create({staffId,Name,code,staffConfirm,phone}, function(err, result){
+  			if(result){
+  				console.log(result);
+  				res.json({
+  					"messages": [
+  					{"text": "Staff Created Successfully"}
+  					]
+  				})
+  			}
+  		})
+  	}else{
+  		res.json({
+  			"messages":[
+  			{"text": "This Staff is already existed"}
+  			]
+  		})
+  	}
+  })
+
+
+
 })
 
 module.exports = router;
---
+++ schema/StaffCreate.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const staff = new Schema({
+    staffId: {
+        type: Number,
+        required: true
+    },
+    Name: {
+    	type: String,
+    	required: true
+    },
+    phone: {
+        type: Number,
+        required: true
+    }
+    code:{
+        type:String,
+        required: true
+    },
+    staffConfirm: {
+    	type: Boolean,
+    	required: true
+    }
+})
+
+
+module.exports = mongoose.model("staff", staff);
+
+
