8cd52d879d824ecc01fd0d6d6a6f182144b71a62
hnin wai <hninwaioo56@gmail.com>
2019-04-09 17:19:23 +0630

    allowed

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -243,6 +243,8 @@ rotuer.post('/check-debt', function(req, res) {
   let name = req.body.name;
   let messengerId = req.body["messenger user id"];
 
+  console.log(`Name is ${name}, messenger ${messengerId}`)
+
   AdminMessenger.find({messengerId}, function(err, admin){
     if(admin[0]=== undefined){
       res.json({
@@ -251,6 +253,7 @@ rotuer.post('/check-debt', function(req, res) {
         ]
       })
     }else{
+      console.log(`Allowed`)
       supplier.find({name: {$regex: name}}, function(err, supplier) {
         console.log(`Supplier is ${supplier}`)
         if(supplier[0] === undefined){
