a87d1e00b1d7a51e22c30ee99a18b315451ef1f2
Albert Veomas <aungmyintmyatiris@gmail.com>
2019-04-10 00:33:27 +0630

    member

--- routes/staff/index.js
+++ routes/staff/index.js
@@ -140,7 +140,7 @@ router.post('/give-point', function(req,res){
 			})
 		}else{
 			console.log(`Customer is ${customers}`);
-			Member.updateOne({memberId:customers.memberId}, {$set: {point}, function(err, members){
+			Member.updateOne({memberId}, {$set: {point}, function(err, members){
 				console.log(`Memebers is ${members}`);
 				if(members){
 					res.json({
