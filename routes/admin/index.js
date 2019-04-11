1881c5e2601b6f4dcbe1b6cbf68bf25502e6c687
Albert Veomas <aungmyintmyatiris@gmail.com>
2019-04-11 20:04:17 +0630

    re

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -69,7 +69,6 @@ router.post('/register', function (req, res) {
         }
         res.json({
           "messages": [
-            {"text": "Admin Register Success"},
             {"redirect_to_blocks": ["Admin"]}
           ]
         })
